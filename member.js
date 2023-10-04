1c85697e40f147a7df9b0cb5a4b9fa7b2218723c
Jeseap <117468959+Jeseap@users.noreply.github.com>
2023-10-04 04:28:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberOption = document.createElement("option");
+  memberOption.value = "Member";
+  memberOption.text = "Member";
+  member.add(memberOption);
+}
